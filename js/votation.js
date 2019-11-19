8993b6a52648215ee3285dcfc85f4e949eb1b5df
daztery <daztery@gmail.com>
2019-11-18 20:52:58 -0500

    Test

--- js/votation.js
+++ js/votation.js
@@ -1,35 +1,26 @@
 const btnSend = document.getElementById("1");
+const url = 'http://localhost:9002/vote/broadcast';
+const headers = new Headers({
+    'Content-Type': 'application/json'
+});
+const body = { 
+    votername: "123",
+    candidateid: "1",
+    candidatename: "PPK2"
+};
 
 btnSend.onclick = e => {
-    const body = { 
-        votername: "72861219",
-        candidateid: "1",
-        candidatename: "PPK2"
-    };
+    
     console.log(JSON.stringify(body));
-    const headers = new Headers({
-        "Content-Type": "application/json"
-    });
-    fetch('http://localhost:9002/vote/broadcast',{
+    
+    fetch(url,{
         method: 'POST',
         headers,
         body: JSON.stringify(body)
-    }).then(response => {
-        if(response.ok){
-            return response.json();
-        }
-        throw new Error('Request Failed!');
-    },networkError => console.log(networkError.message))
-    .then(jsonResponse => {
-        votername = "";
-        candidateid = "";
-        candidatename = "";
-        Swal.fire(
-            'Listo',
-            'Tu mensaje ha sido enviado satisfactoriamente',
-            'success')
-        console.log(jsonResponse);
-    });
-};
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
 
+
+};
 
